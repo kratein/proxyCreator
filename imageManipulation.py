a1d239191b0440f0eee9a8e8c08b5068277feed3
therry <manquedinspiration@gmail.com>
2025-03-02 00:14:30 +0100

    Refactor image manipulation code.
    Add comments.
    Try to change image position to avoid the (fucking) vertical offset.

--- imageManipulation.py
+++ imageManipulation.py
@@ -1,34 +1,38 @@
+import math
 from PIL import Image
 
-X_POSITION_PX = [121,867,1613,121,867,1613,121,867,1613]
-Y_POSITION_PX = [194,194,194,1234,1234,1234,2274,2274,2274]
-WIDTH_MM =  63.10 
-HEIGHT_MM =  88.09  
-DPI = 300
-ONE_INCH_MM = 25.4
-width_px = int((WIDTH_MM / ONE_INCH_MM) * DPI)
-height_px = int((HEIGHT_MM / ONE_INCH_MM) * DPI)
+DPI = 300.0  # Dots Per Inch
+ONE_INCH_MM = 25.4  # 1 inch = 2.54 cm = 25.4 mm
 
-def mm_to_pixels(mm, dpi):
-    return int((mm / ONE_INCH_MM) * dpi)
+
+def mm_to_pixels(mm):
+    return (mm / ONE_INCH_MM) * DPI
+
+
+X_POSITION_PX = [121, 867, 1613, 121, 867, 1613, 121, 867, 1613]
+Y_POSITION_PX = [184, 184, 184, 1225, 1225, 1225, 2265, 2265, 2265]
+IMAGE_WIDTH_MM = 63.10
+IMAGE_HEIGHT_MM = 88.09
+image_width_px = math.floor(mm_to_pixels(IMAGE_WIDTH_MM))  # convert mm to pixel
+image_height_px = math.floor(mm_to_pixels(IMAGE_HEIGHT_MM))  # convert mm to pixel
+width_a4_px = math.floor(mm_to_pixels(210.0))  # 210.0 : width a4 mm -> for 300 DPI, should be 2480 pixels
+height_a4_px = math.ceil(mm_to_pixels(297.0))  # 297.0 : height a4 mm -> for 300 DPI, should be 3508 pixels
+
 
 def resize_all_in_folder(directory: str, lstImage: list):
     for image in lstImage:
         lstPath = str(image).split("/")
-        name = lstPath[len(lstPath)-1]
+        name = lstPath[len(lstPath) - 1]
         image = Image.open(image)
-        image_resized = image.resize((width_px, height_px))
-        image_resized.save(f"{directory}/{name}", dpi=(DPI,DPI))
+        image_resized = image.resize((image_width_px, image_height_px))
+        image_resized.save(f"{directory}/{name}", dpi=(DPI, DPI))
+
 
 def create_page_to_print_front(pathResult: str, lstImages: list):
-    width_a4_mm = 210
-    height_a4_mm = 297
-    width_a4_px = mm_to_pixels(width_a4_mm, DPI)
-    height_a4_px = mm_to_pixels(height_a4_mm, DPI)
     background = Image.new('RGBA', (width_a4_px, height_a4_px), (255, 255, 255, 0))
     i = 0
     for imagePath in lstImages:
         image = Image.open(imagePath)
         background.paste(image, (X_POSITION_PX[i], Y_POSITION_PX[i]), image)
         i = i + 1
-    background.save(pathResult, dpi=(DPI,DPI))
+    background.save(pathResult, dpi=(DPI, DPI))
